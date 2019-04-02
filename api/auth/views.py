509262696df96cff150d8d2ee7bf0189c0c52265
ywager <441200288@qq.com>
2019-04-02 10:11:00 +0800

    Add user authentication
    1. Add JWT to the project
    2. Add login view

--- api/auth/views.py
+++ api/auth/views.py
@@ -1,6 +1,32 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from django.contrib.auth import authenticate, login
 
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework_jwt.settings import api_settings
+from rest_framework.views import status
 
-# Create your views here.
+from serializers import TokenSerializer
+
+jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+
+
+class LoginView(generics.CreateAPIView):
+
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, *args, **kwargs):
+        username = request.data.get("username", "")
+        password = request.data.get("password", "")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            serializer = TokenSerializer(data={
+                "token": jwt_encode_handler(
+                    jwt_payload_handler(user)
+                )})
+            serializer.is_valid()
+            return Response(serializer.data)
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
